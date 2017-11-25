0e32a37e7abe98c3e01b36b93d43f5b31c56f22f
habibaudu <auduhabib1990@gmail.com>
2017-11-25 11:54:35 +0100

    modified index.js,added a user model.user.js,added an index.js in models from sequelize,added an events model event.js added a migration folder to run migrations.added a new controller user.js,index.js.added config folder to hold secreteke, added config.js,for database configuration, added auth folder for authentication

--- server/server/routes/index.js
+++ server/server/routes/index.js
@@ -1,27 +1,14 @@
-import addeventControls from '../controllers/createEvents';
-import geteventControls from '../controllers/getEvents';
-import editeventControls from '../controllers/editevent';
-import deleteeventControls from '../controllers/deleteEvent';
-import addcenterControls from '../controllers/createCenter';
-import getcenterControls from '../controllers/allCenter';
-import getacenterControls from '../controllers/aCenter';
-import updatecenterControls from '../controllers/updateCenter';
+import controller from '../controllers';
 
+const userControllers = controller.Users;
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
-    message: 'Welcome to the EventManager API!',
+    message: 'Welcome to EventManager App!',
   }));
-  app.get('/events', geteventControls.getEvents);
-  app.post('/events', addeventControls.create);
-  app.put('/events/:eventId', editeventControls.eventUpdate);
-  app.delete('/events/:eventId', deleteeventControls.delete);
-  app.post('/centers', addcenterControls.create);
-  app.get('/centers', getcenterControls.getCenters);
-  app.get('/centers/:centerId', getacenterControls.getCenter);
-  app.put('/centers/:centerId', updatecenterControls.centerUpdate);
 
+  app.post('/users', userControllers.register);
 
-  app.all('/center/:centerId/', (req, res) =>
+  app.all('/api/signup/users', (req, res) =>
     res.status(405).send({
       message: 'Method Not Allowed',
     }));
