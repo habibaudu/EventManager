cb335f087cb9086879f8f52a9194e87e8e353185
habibaudu <auduhabib1990@gmail.com>
2017-11-25 17:25:27 +0100

    modified signin.js and index.js controller to get it workin

--- server/server/routes/index.js
+++ server/server/routes/index.js
@@ -1,13 +1,15 @@
 import controller from '../controllers';
 
+
+const signinControllers = controller.signin;
 const signupControllers = controller.signup;
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
     message: 'Welcome to EventManager App!',
   }));
 
-  app.post('/users', signupControllers.register);
-  app.post('/users/', signupControllers.register);
+  app.post('/users',signupControllers.register);
+  app.post('/users/login',signinControllers.login);
 
   app.all('/api/signup/users', (req, res) =>
     res.status(405).send({
