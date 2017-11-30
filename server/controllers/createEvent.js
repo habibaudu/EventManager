f08a2920cb1a0680efcb101295526918baea53c6
habibaudu <auduhabib1990@gmail.com>
2017-11-30 11:25:59 +0100

    fix createevent controller

--- server/controllers/createEvent.js
+++ server/controllers/createEvent.js
@@ -1,13 +1,14 @@
 import models from '../models';
 
-const Event = models.Events;
+const { Events } = models;
 
 export default {
   createEvents(req, res) {
+    const eventPayload = req.body;
+    const { userId, eventType, centerId, eventDate } = eventPayload;
     console.log(req.body);
-    const { eventType, centerId, dates } = req.body;
-    return Event
-      .create({ eventType, centerId, dates })
+    Events
+      .create({ eventType, centerId, eventDate, userId })
       .then(events => res.status(201).send(events))
       .catch(error => res.status(400).send(error));
   },
