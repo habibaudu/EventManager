64b58f1180367092ad12ec481f191aeac5a0eb1e
habibaudu <auduhabib1990@gmail.com>
2017-12-02 09:02:21 +0100

    clear commended code

--- server/controllers/deleteEvents.js
+++ server/controllers/deleteEvents.js
@@ -2,35 +2,6 @@ import models from '../models';
 
 const { Events } = models;
 
-// export default {
-//   destroy(req, res) {
-//     return Events
-//       .findById(req.params.eventId)
-//       .then((event) => {
-//         if (!event) {
-//           return res.status(404).send({
-//             message: 'No event By that Id'
-//           });
-//         }
-//         if (req.decoded.userId === req.event.userId) {
-//           return event
-//             .destroy()
-//             .then(() => res.status(200).json({
-//               message: 'Event has been Deleted!'
-//             }))
-//             .catch(error => res.status(400).json({
-//               message: error.errors[0].message
-//             }));
-//         }
-//         return res.status(401).json({
-//           message: 'You do not have  Authorization to delete this event!'
-//         });
-//       })
-//       .catch(() => res.status(500).json({
-//         message: 'Something went wrong with the server'
-//       }));
-//   }
-// };
 export default {
   destroy(req, res) {
     console.log(req.decoded.id);
